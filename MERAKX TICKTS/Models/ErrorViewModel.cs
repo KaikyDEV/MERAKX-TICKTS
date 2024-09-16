68b9b97398bec2f4e3d063235c1bc9ff0cd6091c
Kaiky - Santos <126119808+KaikyDEV@users.noreply.github.com>
2024-09-16 13:28:43 -0300

    Adicionar arquivos de projeto.

---
+++ MERAKX TICKTS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MERAKX_TICKTS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
